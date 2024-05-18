d6d03940b23de3ebfcb983d3614237d4e5a86fc2
Amandeepsingh25 <amandeepnarang179@gmail.com>
2024-05-18 06:02:23 +0530

    main.tf updated again 2.3

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster"  
 }
+
 # Define IAM role for ECS task execution
 resource "aws_iam_role" "my_task_execution_role" {
   name               = "my-task-execution-role"
@@ -29,6 +30,50 @@ resource "aws_iam_policy_attachment" "ecs_task_execution_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# Define IAM policy for ECR permissions
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ECRPermissionsForTask"
+  description = "Policy for ECR permissions"
+  policy      = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:DescribeImages",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+# Define IAM policy for ECR Image Builder permissions
+resource "aws_iam_policy" "ecr_image_builder_policy" {
+  name        = "ECRImageBuilderPermissionsForTask"
+  description = "Policy for ECR Image Builder permissions"
+  policy      = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "imagebuilder:GetComponent",
+        "imagebuilder:GetContainerRecipe",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchGetImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:PutImage"
+      ],
+      Resource = "*"
+    }]
+  })
+}
 
 # Define ECS task definition
 resource "aws_ecs_task_definition" "my_task" {
@@ -83,24 +128,61 @@ resource "aws_subnet" "subnet2" {
   availability_zone = "ap-south-1b"
 }
 
-resource "aws_security_group" "nodejs_app_sg" {
-  name        = "nodejs-app-sg"
-  description = "Security group for Node.js app running on port 3000"
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_gateway" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Define Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_gateway.id
+  }
+}
+
+# Associate Route Table with Subnets
+resource "aws_route_table_association" "subnet1_association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_route_table_association" "subnet2_association" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Define Security Group
+resource "aws_security_group" "ecs_security_group" {
+  name        = "ecs-security-group"
+  description = "Security group for ECS task"
 
   vpc_id = aws_vpc.my_vpc.id 
 
+  # Inbound rule to allow traffic from the ECR endpoint on port 443
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound rule to allow traffic on port 3000
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Outbound rule allowing traffic to the ECR endpoint
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]  # Replace with the ECR endpoint IP address or CIDR block
   }
 }
 
@@ -113,7 +195,7 @@ resource "aws_ecs_service" "my_service" {
   launch_type     = "FARGATE"  
   network_configuration {
     subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]  
-    security_groups  = [aws_security_group.nodejs_app_sg.id]  
+    security_groups  = [aws_security_group.ecs_security_group.id]  
     assign_public_ip = true  
   }
 }
